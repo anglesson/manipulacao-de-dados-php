fc0917610e480cc6da8cb2a808cbefaade6be4ff
Anglesson Araujo <anglesson@hotmail.com>
2021-06-20 12:44:57 -0300

    Classe TCriteria adicionada

---
+++ app.ado/TCriteria.class.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Classe TCriteria
+ * Esta classe provê uma interface utilizada para definição de critérios
+ */
+class TCriteria extends TExpression
+{
+  private $expressions; // armazena a lista de expressões
+  private $operators; // armazena a lista de operadores
+  private $properties; // propriedades do critério
+
+  /**
+   * Método construtor
+   */
+  function __construct()
+  {
+    $this->expressions = array();
+    $this->operators = array();
+  }
+
+  /**
+   * Método add()
+   *  adiciona uma expressão ao critério
+   * @param $expression = expressão (objeto TExpression)
+   * @param $operator = operador logico de comparação
+   */
+  public function add(TExpression $expression, $operator = self::AND_OPERATOR)
+  {
+    // na primeira vez, não precisamos de operador lógico para concatenar
+    if (empty($this->expressions)) 
+    {
+      $operator = NULL;
+    }
+
+    // agrega o resultado da expressão à lista de expressções
+    $this->expressions[] = $expression;
+    $this->operators[] = $operator;
+  }
+
+  /**
+   * Método dump()
+   * retorna a expressão final
+   */
+  public function dump()
+  {
+    // concatena a lista de expressões
+    if (is_array($this->expressions)) {
+      $result = '';
+      foreach ($this->expressions as $i => $expression)
+      {
+        $operator = $this->operators[$i];
+        // concatena o operador com a respectiva expressão
+        $result .= $operator. $expression->dump() . ' ';
+
+      }
+      $result = trim($result);
+      return "({$result})";
+    }
+  }
+
+  /**
+   * método setProperty()
+   * define o valor de uma propriedade
+   * @param $property = propriedade
+   * @param $value = valor
+   */
+  public function setProperty($property, $value)
+  {
+    if (isset($value)) 
+    {  
+      $this->properties[$property] = $value;
+    }
+    else
+    {
+      $this->properties[$property] = NULL;
+    }
+  }
+
+  /**
+   * método getProperty()
+   * retorna o valor de uma propriedade
+   * 
+   * @param $property = propriedade
+   */
+  public function getProperty($property)
+  {
+    if (isset($this->properties[$property])) 
+    {
+      return $this->properties[$property];
+    }
+  }
+
+}
+
+
+?>
---
+++ criteria.php
@@ -0,0 +1,22 @@
+<?php
+
+include_once "app.ado/TExpression.class.php";
+include_once "app.ado/TCriteria.class.php";
+include_once "app.ado/TFilter.class.php";
+
+// aqui vemos um exemplo de critério utilizando operador lógico OR
+// a idade deve ser menor que 16 OU maoir que 60
+$criteria = new TCriteria;
+$criteria->add(new TFilter('idade', '<', 16), TExpression::OR_OPERATOR);
+$criteria->add(new TFilter('idade', '>', 60), TExpression::OR_OPERATOR);
+echo $criteria->dump();
+echo "\n";
+
+// aqui vemos um exemplo de critério utilizando operador lógico AND
+// juntamente com os operadores IN, (dentro do conjunto) e NOT IN (fora do conjunto)
+// a idade deve estar dentro do conjunto (24, 25, 26) e deve estar fora do conjunto (10)
+$criteria = new TCriteria;
+$criteria->add(new TFilter('idade', 'IN', array(24, 25, 26)));
+$criteria->add(new TFilter('idade', 'NOT IN', array(10)));
+echo $criteria->dump();
+echo "\n";
